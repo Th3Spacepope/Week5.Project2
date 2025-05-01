d443779b9bf92dba58216d0a48acd9e4fe3743ba
Th3Spacepope <tun44465@temple.edu>
2025-05-01 14:02:49 -0400

    Fixed

--- MusicTools.js
+++ MusicTools.js
@@ -1,26 +1,17 @@
-//get HTML Elements
-let midiInput = document.getElementById("midiInput");
-let freqInput = document.getElementById("freqInput");
-let dbfsInput = document.getElementById("dbfsInput");
-let linearInput = document.getElementById("linearInput");
-let freqResult = document.getElementById("freqResult");
-let midiResult = document.getElementById("midiResult");
-let linearResult = document.getElementById("linearResult");
-let dbfsResult = document.getElementById("dbfsResult");
 //functions to do the things
 export let musicTools = {
-  midiPitchToFrequency: function () {
-    freqResult = 440 * Math.pow(2, (midiInput - 69) / 12);
+  midiPitchToFrequency: function (midiInput) {
+    return 440 * Math.pow(2, (midiInput - 69) / 12);
   },
-  frequencyToMidiPitch: function () {
-    midiResult = 69 + 12 * Math.log2(freqInput / 440);
+  frequencyToMidiPitch: function (freqInput) {
+    return 69 + 12 * Math.log2(freqInput / 440);
   },
-  linearToDbfs: function () {
-    linearResult = Math.pow(10, dbfsInput / 20);
+  dbfsToLinear: function (dbfsInput) {
+    return Math.pow(10, dbfsInput / 20);
   },
-  dbfsToLinear: function () {
-    dbfsResult = 20 * Math.log10(linearInput);
+  linearToDbfs: function (linearInput) {
+    return 20 * Math.log10(linearInput);
   },
 };
 
-export const midiPitchToFrequency = function () {};
+export default musicTools;
--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 import { musicTools } from "./MusicTools.js";
 
-//get HTML elements
+const mToF = function () {
+  let result = musicTools.midiPitchToFrequency(midiInput.value);
+  freqResult.innerText = result;
+};
 
+const fToM = function () {
+  let result = musicTools.frequencyToMidiPitch(freqInput.value);
+  midiResult.innerText = result;
+};
+
+const dBToA = function () {
+  let result = musicTools.dbfsToLinear(dbfsInput.value);
+  linearResult.innerText = result;
+};
+
+const aTodB = function () {
+  let result = musicTools.linearToDbfs(linearInput.value);
+  dbfsResult.innerText = result;
+};
+//get HTML Elements
+let midiInput = document.getElementById("midiInput");
+let freqInput = document.getElementById("freqInput");
+let dbfsInput = document.getElementById("dbfsInput");
+let linearInput = document.getElementById("linearInput");
+let freqResult = document.getElementById("freqResult");
+let midiResult = document.getElementById("midiResult");
+let linearResult = document.getElementById("linearResult");
+let dbfsResult = document.getElementById("dbfsResult");
 let midiToFreqButton = document.getElementById("midiToFreq");
 let freqToMidiButton = document.getElementById("freqToMidi");
 let dbfsToLinearButton = document.getElementById("dbfsToLinear");
 let linearToDbfsButton = document.getElementById("linearToDbfs");
 
 //event listeners
-midiToFreqButton.addEventListener("click", musicTools.midiPitchToFrequency());
-freqToMidiButton.addEventListener("click", musicTools.frequencyToMidiPitch());
-dbfsToLinearButton.addEventListener("click", musicTools.dbfsToLinear());
-linearToDbfsButton.addEventListener("click", musicTools.linearToDbfs());
+midiToFreqButton.addEventListener("click", mToF);
+freqToMidiButton.addEventListener("click", fToM);
+dbfsToLinearButton.addEventListener("click", dBToA);
+linearToDbfsButton.addEventListener("click", aTodB);
